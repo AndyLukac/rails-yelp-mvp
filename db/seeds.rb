fe7c83f237a18c6dfb8a864492865ec767616241
Andrew Lukac a.lukac28@gmail.com <a.lukac28@gmail.com>
2018-08-15 12:16:12 +0200

    Rails new with frontend and form gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone:  '0653476516',
+    reviews:       'Food was cold.'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone:  '0629476558',
+    reviews:        'Food was delicous.'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
